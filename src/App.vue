2406372fa61ff8f39a4acffc8042aaaaa743de55
ingenieria-web-proyecto <katherinesanhueza@alu.unach.cl>
2026-09-22 17:47:47 -0300

    Actividad 3 - Emit desde hijo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div id="app">
+    <Padre />
+  </div>
 </template>
+
+<script>
+import Padre from './components/Padre.vue'
+
+export default {
+  components: {
+    Padre
+  }
+}
+</script>
